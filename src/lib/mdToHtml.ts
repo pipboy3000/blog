251c0f58ff676cb871da074216a7f1e7a9709301
Asai Masami <shakeforme@gmail.com>
2023-03-24 23:25:33 +0900

    コードハイライトが有効になるよう修正

--- src/lib/mdToHtml.ts
+++ src/lib/mdToHtml.ts
@@ -1,8 +1,8 @@
 import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkGfm from 'remark-gfm';
-import rehypeHighlight from 'rehype-highlight'
-import rehypeSanitize from 'rehype-sanitize';
+import rehypeHighlight from 'rehype-highlight';
+import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
 import rehypeStringify from 'rehype-stringify';
 import remarkRehype from 'remark-rehype';
 
@@ -10,12 +10,63 @@ const mdToHtml = async (markdown: string) => {
   const file = await unified()
     .use(remarkParse)
     .use(remarkGfm)
-		.use(remarkRehype)
-		.use(rehypeSanitize)
-		.use(rehypeHighlight)
-		.use(rehypeStringify)
+    .use(remarkRehype)
+    .use(rehypeHighlight)
+    .use(rehypeSanitize, {
+      ...defaultSchema,
+      attributes: {
+        ...defaultSchema.attributes,
+        span: [
+          ...(defaultSchema.attributes?.span || []),
+          [
+            'className',
+            'hljs-addition',
+            'hljs-attr',
+            'hljs-attribute',
+            'hljs-built_in',
+            'hljs-bullet',
+            'hljs-char',
+            'hljs-code',
+            'hljs-comment',
+            'hljs-deletion',
+            'hljs-doctag',
+            'hljs-emphasis',
+            'hljs-formula',
+            'hljs-keyword',
+            'hljs-link',
+            'hljs-literal',
+            'hljs-meta',
+            'hljs-name',
+            'hljs-number',
+            'hljs-operator',
+            'hljs-params',
+            'hljs-property',
+            'hljs-punctuation',
+            'hljs-quote',
+            'hljs-regexp',
+            'hljs-section',
+            'hljs-selector-attr',
+            'hljs-selector-class',
+            'hljs-selector-id',
+            'hljs-selector-pseudo',
+            'hljs-selector-tag',
+            'hljs-string',
+            'hljs-strong',
+            'hljs-subst',
+            'hljs-symbol',
+            'hljs-tag',
+            'hljs-template-tag',
+            'hljs-template-variable',
+            'hljs-title',
+            'hljs-type',
+            'hljs-variable',
+          ],
+        ],
+      },
+    })
+    .use(rehypeStringify)
     .process(markdown);
   return file.toString();
 };
 
-export default mdToHtml
+export default mdToHtml;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,11 @@
 import type { InferGetStaticPropsType, NextPage } from 'next';
 import { getAllPosts } from '../lib/api';
-import { generateIndex } from '../lib/algolia';
 import PostItem from '../components/PostItem';
 import PageLayout from '../components/PageLayout';
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 export const getStaticProps = async () => {
-  if (process.env.NODE_ENV === 'production') {
-    await generateIndex();
-  }
-
   const allPosts = getAllPosts();
   return {
     props: { allPosts },
